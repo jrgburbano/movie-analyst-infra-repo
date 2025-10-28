2cb5fa165a79e2dda755a883c522f44c448e9c57
Jorge Andrés Burbano Muñeton <jrgburbano@yahoo.com>
2025-10-28 13:18:56 -0500

    Private & Public Subnets created

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ module "vpc" {
   cidr_block = var.vpc_cidr
   vpc_name   = "main-vpc"
 }
+
+module "subnets" {
+  source       = "./modules/subnets"
+  vpc_id       = module.vpc.vpc_id
+  public_cidr  = var.public_subnet_cidr
+  private_cidr = var.private_subnet_cidr
+  az           = var.az
+  project      = "movie-analyst"
+}
